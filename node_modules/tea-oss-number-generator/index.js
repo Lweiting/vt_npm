3c2683fb041a2070851e5f263f95e25d1cb77af6
Lweiting <50763914+Lweiting@users.noreply.github.com>
2024-02-24 12:32:34 +0800

    Delete node_modules directory

--- node_modules/tea-oss-number-generator/index.js
+++
@@ -1,7 +0,0 @@
-function randomNoGenerator(min, max) {
-  if(typeof(max) !== 'number' && typeof(min) !== 'number') {
-    min = 0;  max = 1;
-  }
- return (Math.random() * (max-min)) + min;
-}
-module.exports = randomNoGenerator;
